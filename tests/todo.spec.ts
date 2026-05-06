58ccfc8eb676fd34e864aadc8b0651a6f6a366c9
Aleksandr Liadov <33938644+avlin87@users.noreply.github.com>
2026-05-06 08:45:23 -0700

    add Page Object

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,50 +1,28 @@
-import { test, expect, Page } from '@playwright/test';
+import { test } from '@playwright/test';
+import { TodoPage } from '../pages/TodoPage';
+
+let todoPage: TodoPage;
 
 test.beforeEach(async ({ page }) => {
+  todoPage = new TodoPage(page);
   await page.goto('/todomvc');
 });
 
-test('adds todo items and updates counter', async ({ page }) => {
-  await addTodoItem(page, 'Learn Playwright');
-  await assertTodoVisibleAndCounter(page, 'Learn Playwright', '1 item left', 1);
+test('adds todo items and updates counter', async ({ }) => {
+  await todoPage.addTodoItem('Learn Playwright');
+  await todoPage.assertTodoVisibleAndCounter('Learn Playwright', '1 item left', 1);
 
-  await addTodoItem(page, 'Become Expert');
-  await assertTodoVisibleAndCounter(page, 'Learn Playwright', '2 items left', 2);
-  await assertTodoVisibleAndCounter(page, 'Become Expert', '2 items left', 2);
-  
+  await todoPage.addTodoItem('Become Expert');
+  await todoPage.assertTodoVisibleAndCounter('Learn Playwright', '2 items left', 2);
+  await todoPage.assertTodoVisibleAndCounter('Become Expert', '2 items left', 2);
+
 });
 
-test('marks todo item as completed', async ({ page }) => {
-  await addTodoItem(page, 'Learn Playwright');
-  await assertTodoVisibleAndCounter(page, 'Learn Playwright', '1 item left', 1);
+test('marks todo item as completed', async ({ }) => {
+  await todoPage.addTodoItem('Learn Playwright');
+  await todoPage.assertTodoVisibleAndCounter('Learn Playwright', '1 item left', 1);
 
-  await completeTodoItem(page, 'Learn Playwright');
+  await todoPage.completeTodoItem('Learn Playwright');
 
-  await assertTodoVisibleAndCounter(page, 'Learn Playwright', '0 items left', 1);
+  await todoPage.assertTodoVisibleAndCounter('Learn Playwright', '0 items left', 1);
 });
-
-async function addTodoItem(page: Page, text: string) {
-  await page
-    .getByPlaceholder('What needs to be done?')
-    .fill(text);
-  await page.keyboard.press('Enter');
-}
-
-async function assertTodoVisibleAndCounter(page: Page, todoItem: string, itemCount: string, expectedTodoCount: number) {
-  await expect(todoItems(page).filter({ hasText: todoItem })).toBeVisible();
-  await expect(page.locator('.footer').getByText(itemCount)).toBeVisible();
-  await expect(todoItems(page)).toHaveCount(expectedTodoCount);
-}
-
-async function completeTodoItem(page: Page, todoItem: string) {
-  const todo = todoItems(page)
-    .filter({ hasText: todoItem });
-  await todo
-    .getByLabel('Toggle Todo')
-    .setChecked(true);
-  await expect(todo).toHaveClass(/completed/);
-}
-
-function todoItems(page: Page) {
-  return page.locator('.todo-list').getByRole('listitem');
-}
